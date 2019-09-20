67a860ee39aaab873d311abeff48e8817a77eaca
Prakhar Sharan <itsmeprakhar22@gmail.com>
2019-09-21 03:01:47 +0530

    Implemented Timer Toast which is lifecycle aware using ViewModel and Lifecycle

--- app/src/main/java/com/example/mvvmbasics/MainActivity.kt
+++ app/src/main/java/com/example/mvvmbasics/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.mvvmbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var timerToast:TimerToast
-
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        timerToast = TimerToast(application, this)
+        viewModel= ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.timerData.observe(this, Observer {
+            Toast.makeText(applicationContext, it, Toast.LENGTH_SHORT).show()
+        })
+        viewModel.start()
     }
 }
